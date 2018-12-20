b06be222033ffe1c64f2f0b4b68ccfa0561ac52e
Weijun Huang <wjhuang0726@gmail.com>
2018-12-20 18:05:37 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -18,6 +18,22 @@ end
 
 def apply_coupons(cart, coupons)
   # code here
+  def apply_coupons(cart, coupons)
+  # code here
+  hash={}
+  cart.each do |k, v|
+    coupons.each do |s, t|
+      if k == t
+      v[:count]=cart[k][:count]-coupons[:num]
+        hash["#{k} W/COUPONS"]={:price => coupons[:cost], 
+                                :clearance => cart[k][:clearance],
+                                :count =>v[:count]  }
+      end
+      hash[k]=v
+    end
+  end
+  hash
+end
 end
 
 def apply_clearance(cart)
